2679debf618ba1a2e991701b7d012d5398a78f7a
Daniel Ospina <do7269301@gmail.com>
2025-09-22 14:14:26 -0500

    Genericos y Toastify4

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -348,3 +348,16 @@
 //   console.log(result);
 
 //   return result;
+
+
+import React from 'react'
+
+
+const Index2 = () => {
+  return (
+    <div>index2</div>
+  )
+}
+
+
+export default Index2
