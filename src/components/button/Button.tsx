7da430e02b9fa2fc5e4164c587a584732e9e8dd7
Daniel Ospina <do7269301@gmail.com>
2025-09-25 14:27:04 -0500

    componentes reutilizables

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,20 +2,29 @@ import { JSX } from "react";
 
 interface MiButtonProps {
   text: string;
-  next: string;
   icon: string | JSX.Element;
+  disabled?: boolean;
+  loading?: boolean;
+  click?: () => void;
+  variant?: "primary" | "secondary" | "danger";
+  size?: "sm" | "md" | "lg";
 }
 
-export const MiButton = ({ text, next, icon }: MiButtonProps) => {
-  const handleClick = () => {
-    console.log("Se hizo click en el boton del componente");
-  };
+export const MiButton = ({text,icon,disabled = false, loading = false, click, variant = "primary", size}: MiButtonProps) => {
+  
 
   return (
-    <button onClick={handleClick} className="components--button">
+    <button
+      onClick={click}
+      className={`components__button ${
+        disabled || loading ? "components__button--disabled" : ""
+      } ${variant === "secondary" ? "components__button--secondary": variant === "danger" ? "components__button--danger": variant ? "components__button--primary" : ""  }` }
+
+      disabled={disabled || loading}
+    >
       <div className="flex flex-row items-center gap-2">
-        <div className="flex flex-col gap-2">
-          {text} {next}
+        <div className="flex flex-col gap-2" style={{fontSize: size === "sm" ? "12px" : size === "md" ? "20px" : size === "lg" ? "30px" : "16px"}}>
+          {loading ? "Cargando..." : text} 
         </div>
 
         <div>{icon}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,15 @@
 // import { useState } from "react";
 // import { user } from "./api/hello";
 import { Card } from "@/card/Card";
+import { MiButton } from "@/components/button/Button";
+import { notification } from "@/helpers/utils";
+import { useState } from "react";
 // import { MiButton } from "@/components/button/Button";
 // import { notification } from "@/helpers/utils";
 // import { ToastContainer } from "react-toastify";
 //crear un html con atributo onClick => llama funcion local del componente, puede ser llamdo handleClic, instancie una clase y use un metodo getUsers => previamente debe crear una clase dentro de una carpeta llamada services/users.ts, crea la clase, agrega un atributo con la base url, y crea metodos que llamen al backend con un fetch => el backend debe tener un handler que retorne algo
 import { PiArrowCircleUpRight, PiArrowCircleUpRightFill } from "react-icons/pi";
+import { ToastContainer } from "react-toastify";
 export default function Home() {
   // const handleClickError = async () => {
   //   notification("Sucedio un error", "error", 500);
@@ -29,10 +33,27 @@ export default function Home() {
   //   notification("Boton info", "info", 1000);
   // };
 
+
+  const [loader, setLoader] =  useState(false);
+
+
+   const  handlerClick = () => {
+    setLoader(true);
+
+    setTimeout(() => {
+      setLoader(false);
+    }, 3000);
+  }
+
+  // const handlerNot = () => {
+  //   notification("hola", "success")
+  // }
+
+
   return (
     <>
       {/* <div>{Login()}</div> */}
-      <div className="Container">
+      {/* <div className="Container">
 
           <Card title={"Search engine optimization"} icon={<PiArrowCircleUpRightFill size={20} />} flecha={"#b9ff66"} text={"Learn more"} image={"/img/1.png"} color="gray" background="#B9FF66"/>
           <Card title={"Pay-per-click advertising"}  icon={<PiArrowCircleUpRightFill size={20} />} text={"Learn more"} image={"/img/2.png"} color="green" background="#F3F3F3"/>
@@ -41,7 +62,7 @@ export default function Home() {
           <Card title={"Content Creation"}  icon={<PiArrowCircleUpRightFill size={20} />} text={"Learn more"} image={"/img/5.png"} color="green" background="#F3F3F3"/>
           <Card title={"Analytics and Tracking"} icon={<PiArrowCircleUpRightFill size={20} color="white"/>} text={"Learn more"} image={"/img/6.png"} color="blue" background="#F3F3F3" colorText={"#F3F3F3"}/>
       </div>        
-  
+   */}
 
       {/* <div className="container "> */}
 
@@ -62,15 +83,15 @@ export default function Home() {
           Info!
         </button> */}
 
-        {/* <div className="flex gap-2">
-          <MiButton text={"Search engine optimization"} next="Learn More" icon={""} />
-          <MiButton text={"Pay-per-click advertising"} next="Learn More" icon={"X"} />
-          <MiButton text={"Social Media Marketing"} next="Learn More" icon={">>"} />
-          <MiButton text={"Email Marketing"} next="Learn More" icon={">>"} />
-        </div> */}
+        <div className="flex gap-2">
+          <MiButton text={"cancelar"} icon={"!"} variant="danger" size="lg" />
+          <MiButton text={"guardar"} icon={"X"}  click={() => notification("un texto", "success")} variant="primary" size="md"/>
+          <MiButton text={"siguiente"} icon={">"} click={handlerClick} loading={loader} variant="secondary" size="sm"/>
+        </div>
 
-        {/* <ToastContainer />
+         <ToastContainer />
         {/* <div>
+        {
           <div>Hola mundo</div>
 
           <button className="miButton" onClick={handleClick}>
